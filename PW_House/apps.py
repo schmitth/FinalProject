443ee47755a10fc67893ebb7ac93f8b82341d989
schmitth <harryjschmitt@gmail.com>
2021-12-07 17:35:41 -0500

    Initial

---
+++ PW_House/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PwHouseConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'PW_House'
---
+++ PW_House/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+
+
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'index.html')
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+def images_view(request):
+    return redirect('images')
